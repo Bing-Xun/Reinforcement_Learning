dbf020d43a9a5ba6b9a1fec22ee84d3ecafeb991
bing <wu20022002a@gmail.com>
2025-04-02 13:13:52 +0800

    add mysql

---
+++ DDPG/sql/InitManager.sql
@@ -0,0 +1,2 @@
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root_password';
+FLUSH PRIVILEGES;
--- DDPG/src/main/java/db/config/MyBatisConfig.java
+++ DDPG/src/main/java/db/config/MyBatisConfig.java
@@ -6,28 +6,53 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.yaml.snakeyaml.Yaml;
+import property.ConfigReader;
 
 import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 public class MyBatisConfig {
 
+    private static PooledDataSource dataSource;
+
+    static {
+//        // resource 加載
+//        try {
+//            // 使用 Resources 从类路径加载 mybatis-config.yaml 配置文件
+//            InputStream inputStream = Resources.getResourceAsStream("mybatis-config.yaml");
+//            Yaml yaml = new Yaml();
+//            Map<String, Object> config = yaml.load(inputStream);
+//            Map<String, String> databaseConfig = (Map<String, String>) config.get("database");
+//
+//            // 配置数据源
+//            PooledDataSource dataSource = new PooledDataSource();
+//            dataSource.setDriver(databaseConfig.get("driver"));
+//            dataSource.setUrl(databaseConfig.get("url"));
+//            dataSource.setUsername(databaseConfig.get("username"));
+//            dataSource.setPassword(String.valueOf(databaseConfig.get("password")));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+        try {
+            Properties config = ConfigReader.loadConfig("config.properties");
+
+            if (config != null) {
+                PooledDataSource dataSource = new PooledDataSource();
+                dataSource.setDriver((String) config.get("database.driver"));
+                dataSource.setUrl((String) config.get("database.url"));
+                dataSource.setUsername((String) config.get("database.username"));
+                dataSource.setPassword(String.valueOf(config.get("database.password")));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public SqlSessionFactory sqlSessionFactory() {
         try {
-            // 使用 Resources 从类路径加载 mybatis-config.yaml 配置文件
-            InputStream inputStream = Resources.getResourceAsStream("mybatis-config.yaml");
-            Yaml yaml = new Yaml();
-            Map<String, Object> config = yaml.load(inputStream);
-            Map<String, String> databaseConfig = (Map<String, String>) config.get("database");
-
-            // 配置数据源
-            PooledDataSource dataSource = new PooledDataSource();
-            dataSource.setDriver(databaseConfig.get("driver"));
-            dataSource.setUrl(databaseConfig.get("url"));
-            dataSource.setUsername(databaseConfig.get("username"));
-            dataSource.setPassword(String.valueOf(databaseConfig.get("password")));
-
             // 创建 MyBatis 配置
             org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
             configuration.setEnvironment(new org.apache.ibatis.mapping.Environment("development", new org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory(), dataSource));
